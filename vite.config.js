8b6c435128b375ab9aa8fba4637800910dbfd20b
Kekendr <ashaikin00@mail.ru>
2026-05-14 16:55:57 +0300

    fix: project link

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/history_itmo_project/', 
   plugins: [
     vue(),
     vueDevTools(),
